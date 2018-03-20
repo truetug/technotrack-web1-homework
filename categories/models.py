f0d4b5dbef3e8ed41e48256fa089692677cf1502
guraleks <aleksei.gurov@phystech.edu>
2018-03-20 05:33:35 +0300

    + hw 2

--- categories/models.py
+++ categories/models.py
@@ -4,7 +4,6 @@ from __future__ import unicode_literals
 from django.conf import settings
 from django.db import models
 
-# Create your models here.
 class Category(models.Model):
 
     name = models.CharField(max_length=255, verbose_name=u'Имя категории')
